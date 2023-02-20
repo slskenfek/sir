a5c99a5468ede3718e6ff6d53b66f6e535ad8dea
slskenfek <slskenfek@naver.com>
2023-02-20 17:43:49 +0900

    시큐리티설정

--- src/main/java/com/orders/sir/config/SecurityWebConfig.java
+++ src/main/java/com/orders/sir/config/SecurityWebConfig.java
@@ -1,13 +1,44 @@
 package com.orders.sir.config;
 
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * 스프링 스큐리티 설정
  */
-public class SecurityWebConfig  {
+@Configuration
+public class SecurityWebConfig {
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
 
-
+    /**@deprecated
+     * 모든 필터 해제됨
+     * @return
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/yongjun/**")
+                .anyRequest();
+    }
 
 }
