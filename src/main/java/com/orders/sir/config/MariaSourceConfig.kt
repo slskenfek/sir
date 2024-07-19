25ec72f41ed2fe68f6b829c844e921f90e3313df
yongjun <slskenfek@naver.com>
2024-07-19 09:19:35 +0900

    설정백업

---
+++ src/main/java/com/orders/sir/config/MariaSourceConfig.kt
@@ -0,0 +1,19 @@
+package com.orders.sir.config
+
+import com.zaxxer.hikari.HikariDataSource
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
+
+@Configuration
+class MariaSourceConfig {
+    @Bean
+    @ConfigurationProperties("spring.datasource")
+    fun dataSource(): DataSource {
+        return DataSourceBuilder.create()
+            .type(HikariDataSource::class.java)
+            .build()
+    }
+}
