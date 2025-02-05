65cbd56d088e2f9f1f1ca947c1b230b9a9ade850
DESKTOP-D2PFH23\yongjun <rladydwns248@gmail.com>
2025-02-05 22:25:55 +0900

    아 css html 하기싫다

--- src/main/resources/static/js/content/main.js
+++ src/main/resources/static/js/content/main.js
@@ -1,6 +1,32 @@
 
+        // 상품 로드 함수
+    function loadProducts(startIndex, count) {
+       for (let i = startIndex; i < startIndex + count && i < products.length; i++) {
+                    const productDiv = document.createElement("div");
+                    productDiv.className = "main-product";
+                    productDiv.innerHTML = `<p>${products[i]}</p><button>장바구니에 추가</button>`;
+                    productContainer.appendChild(productDiv);
+         }
+    }
 
-        const products = [
+
+
+function get_content_list() {
+
+    $.ajax({
+           url: '/api/content',
+           method: 'GET',  // 요청 방식 (GET, POST 등)
+           dataType: 'json',  // 응답 데이터 형식 (json, html 등)
+           contentType: 'application/json',
+
+           success: function(response) {
+               console.log(response)
+           },
+           error: function() {
+               alert("실패 하였습니다")
+           }
+       });
+   /*     const products = [
             "상품 1", "상품 2", "상품 3", "상품 4", "상품 5",
             "상품 6", "상품 7", "상품 8", "상품 9", "상품 10",
             "상품 11", "상품 12", "상품 13", "상품 14", "상품 15",
@@ -18,16 +44,10 @@
                 // 스크롤이 페이지 하단에 닿으면 추가 상품 로드
                 loadProducts(productContainer.children.length, loadCount);
             }
-        });
-
-        // 상품 로드 함수
-function loadProducts(startIndex, count) {
-   for (let i = startIndex; i < startIndex + count && i < products.length; i++) {
-                const productDiv = document.createElement("div");
-                productDiv.className = "main-product";
-                productDiv.innerHTML = `<p>${products[i]}</p><button>장바구니에 추가</button>`;
-                productContainer.appendChild(productDiv);
-     }
+        });*/
 }
+
+
+
 
 
